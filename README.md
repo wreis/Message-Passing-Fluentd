d45df54ddd76a01023ba08be1ed15b1531c86cd4
Wallace Reis <wallace@reis.me>
2018-12-13 20:57:49 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Message::Passing::Fluentd - Blah blah blah
+
+# SYNOPSIS
+
+    use Message::Passing::Fluentd;
+
+# DESCRIPTION
+
+Message::Passing::Fluentd is
+
+# AUTHOR
+
+Wallace Reis <wallace@reis.me>
+
+# COPYRIGHT
+
+Copyright 2018- Wallace Reis
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Message/Passing/Fluentd.pm
@@ -0,0 +1,39 @@
+package Message::Passing::Fluentd;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Message::Passing::Fluentd - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Message::Passing::Fluentd;
+
+=head1 DESCRIPTION
+
+Message::Passing::Fluentd is
+
+=head1 AUTHOR
+
+Wallace Reis E<lt>wallace@reis.meE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2018- Wallace Reis
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
